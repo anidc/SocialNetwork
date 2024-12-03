52b0b3281eeb82bbea28eb358495ac316a94268a
anid <anidcamdzic@hotmail.com>
2024-12-03 23:05:01 +0100

    post dtos added

---
+++ SocialNetwork/Dtos/Post/CreatePostDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Dtos.Post
+{
+    public class CreatePostDto
+    {
+        [Required]
+        [MaxLength(2560, ErrorMessage = "Maximum length for the content is 2560 characters.")]
+        public string Content { get; set; }
+
+    }
+}
---
+++ SocialNetwork/Dtos/Post/PostDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Dtos
+{
+    public class PostDto
+    {
+        public string Content { get; set; }
+    }
+}
