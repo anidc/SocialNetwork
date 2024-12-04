0a6863e8f18c9c9c3c9bc0562b7920f4fd4344d7
anid <anidcamdzic@hotmail.com>
2024-12-04 18:34:41 +0100

    post mapper added

---
+++ SocialNetwork/Mappers/PostMapper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SocialNetwork.Dtos;
+using SocialNetwork.Dtos.Post;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Mappers
+{
+    public static class PostMapper
+    {
+        public static PostDto ToPostDto(this Post post)
+        {
+            return new PostDto 
+            { 
+                Content = post.Content
+            };
+        }
+        public static Post ToPostFromPostDto(this CreatePostDto postDto) 
+        {
+            return new Post 
+            { 
+                Content = postDto.Content
+            };
+        } 
+    }
+}
