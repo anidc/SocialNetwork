f042c3eaeb8fa5d164d5498d334aa6f56ec55cc2
anid <anidcamdzic@hotmail.com>
2024-12-08 22:58:09 +0100

    removed unused imports

--- SocialNetwork/Controllers/PostController.cs
+++ SocialNetwork/Controllers/PostController.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using SocialNetwork.Data;
 using SocialNetwork.Dtos.Post;
-using SocialNetwork.Helpers;
 using SocialNetwork.Interfaces;
-using SocialNetwork.Mappers;
-using SocialNetwork.Models;
 
 namespace SocialNetwork.Controllers
 {
