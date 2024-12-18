53a1e42e8bc9baefa6c42ca76ae440e66b0b8fac
anid <anidcamdzic@hotmail.com>
2024-12-18 18:55:30 +0100

    create post

--- SocialNetwork.UI/src/app/components/home/home.component.ts
+++ SocialNetwork.UI/src/app/components/home/home.component.ts
@@ -3,6 +3,9 @@ import { PostsService } from '../../services/posts.service';
 import { Post } from '../../interfaces/post';
 import { CommentsService } from '../../services/comments.service';
 import { Comment } from '../../interfaces/comment';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../../services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -13,23 +16,56 @@ import { Comment } from '../../interfaces/comment';
 })
 export class HomeComponent {
   posts!: Post[];
+  postForm!: FormGroup;
   comments!: Comment[];
   activePostId: number | null = null;
+  isAuthenticated = false;
 
   constructor(
+    private readonly formBuilder: FormBuilder,
     private readonly postService: PostsService,
-    private readonly commentsService: CommentsService
+    private readonly commentsService: CommentsService,
+    public accountService: AccountService,
+    private toastr: ToastrService
   ) {
     this.getAllPosts();
+    this.initPublishPost();
+    this.isAuthenticated = this.accountService.isUserAuthenticated();
   }
 
   getAllPosts() {
     this.postService.getAllPosts().subscribe((posts) => {
       this.posts = posts;
-      console.log(this.posts);
     });
   }
 
+  initPublishPost() {
+    this.postForm = this.formBuilder.group({
+      content: ['', [Validators.required, Validators.maxLength(2560)]],
+    });
+  }
+
+  publishPost() {
+    if (this.postForm.invalid) {
+      console.log('Invalid post');
+      return;
+    }
+
+    const newPost: any = {
+      content: this.postForm.value.content,
+      likes: 0,
+    };
+
+    this.postService.createPost(newPost).subscribe({
+      next: (response) => {
+        this.getAllPosts();
+        this.postForm.reset();
+      },
+      error: (error) => {
+        this.toastr.error(error.error.toString());
+      },
+    });
+  }
   likePost(postId: number) {
     this.postService.toggleLike(postId).subscribe((response) => {
       var post = this.posts.find((post) => post.id === postId);
@@ -40,8 +76,6 @@ export class HomeComponent {
       } else {
         post!.likes -= 1;
       }
-
-      console.log(response);
     });
   }
 
@@ -55,7 +89,6 @@ export class HomeComponent {
         .getAllCommentsByPost(postId)
         .subscribe((newComments: Comment[]) => {
           this.comments = newComments;
-          console.log(this.comments);
         });
     }
   }
